f6578edac9f33efbe4dbea35c3bb15b2e29b4be6
Alisher778 <internet-docs@mail.ru>
2016-12-15 19:12:57 -0500

    added aws3 credintials

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var sequelize = new Sequelize(process.env.DATABASE_URL || databaseURL);
 var fs = require('fs');
 var http = require('http');
 var pg = require('pg');
-
+var aws = require('aws-sdk');
+var S3_BUCKET = process.env.S3_BUCKET;
 
 var multer = require('multer');
 //User profile folder
@@ -79,6 +80,35 @@ var Like = sequelize.define('like',{
 
 Uploads.hasMany(Comments);
 Comments.belongsTo(Uploads);
+
+
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+
+
 
 
 app.get('/', function(req, res) {
