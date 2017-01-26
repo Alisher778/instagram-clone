10d9c3f9274391cbf7d64d62ddea8489bd51ccb8
Alisher778 <internet-docs@mail.ru>
2017-01-26 18:16:01 -0500

    fixed mobile media query

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,5 @@
 $(document).ready(function(){
     
-    //Comments Form submit
-    // $("form.comment").submit(function(event){
-    //    // event.preventDefault()
-    //    var uploadId = event.target.comment.dataset.id;
-    //    var comment = event.target.comment.value;
-
-
-    //    $.ajax({
-    //     type: "POST",
-    //     url: `/comments/${uploadId}/post`,
-    //     data: comment,
-    //     success: function(comments){
-
-    //         console.log( $('#upload-'+comments.uploadId) );
-    //         console.log(comments);
-    //         console.log(comments.comment)
-    //         console.log(comments.id)
-    //         $('#upload-'+comments.uploadId).append(
-    //             '<li>'+
-    //                 '<a href="/users/'+comments.userId+'">'+comments.userEmail+'</a>'+
-    //                 '<span class="comment-span">'+comments.comment+'</span>'+
-    //             '</li>')
-
-    //         }
-    //     })
-        
-    // });
-
-
     // Get All comments accordingly by Uploads id
     $(function(){
         var $comments = $('.comments ul');  
@@ -46,6 +17,18 @@ $(document).ready(function(){
 
     })
 
+   
+        $('img.like').click(function(event){
+            event.preventDefault();
+            alert('clicked');
+
+        var userLike2 = $('img.like').attr('data-upload');
+        
+        console.log(userLike2);
+        });
+        
+
+    
 
 
 });
